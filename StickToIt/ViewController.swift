798d1b074a704b40cd439cf8331fe1a5c1b79951
domb <80871083+DONG-WOON@users.noreply.github.com>
2023-09-20 22:26:43 +0900

    Initial Commit

---
+++ StickToIt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StickToIt
+//
+//  Created by 서동운 on 9/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
