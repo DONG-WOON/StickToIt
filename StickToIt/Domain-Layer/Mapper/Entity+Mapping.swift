cf41c47d41f5bbcbe19c41ea8be7615140658279
domb <80871083+DONG-WOON@users.noreply.github.com>
2023-09-30 20:22:43 +0900

    Feat: Mapper 추가
    - Domain-Layer에 Entity, Model Mapper 추가

---
+++ StickToIt/Domain-Layer/Mapper/Entity+Mapping.swift
@@ -0,0 +1,49 @@
+//
+//  Entity+Mapping.swift
+//  StickToIt
+//
+//  Created by 서동운 on 9/29/23.
+//
+
+import Foundation
+
+extension UserEntity {
+    func toDomain() -> User {
+        return .init(
+            _id: _id,
+            name: name,
+            plans: plans.map { $0.toDomain() }
+        )
+    }
+}
+
+extension PlanEntity {
+    func toDomain() -> Plan {
+        return .init(
+            _id: _id,
+            title: title,
+            targetWeek: targetWeek,
+            startDate: startDate,
+            weeklyPlans: weeklyPlans.map { $0.toDomain() }
+        )
+    }
+}
+
+extension WeeklyPlanEntity {
+    func toDomain() -> WeeklyPlan {
+        return .init(
+            dayPlans: dayPlans.map { $0.toDomain() }
+        )
+    }
+}
+
+extension DayPlanEntity {
+    func toDomain() -> DayPlan {
+        return .init(
+            _id: _id,
+            date: date,
+            imageURL: imageURL,
+            content: content
+        )
+    }
+}
---
+++ StickToIt/Domain-Layer/Mapper/Model+Mapping.swift
@@ -0,0 +1,27 @@
+//
+//  Model+Mapping.swift
+//  StickToIt
+//
+//  Created by 서동운 on 9/29/23.
+//
+
+import Foundation
+//
+//extension User {
+//    func toEntity() -> UserEntity {
+//        return .init(
+//            name: name, plans: plans.map(UserEntity.init))
+//    }
+//}
+//
+//extension Plan {
+//    func toEntity() -> PlanEntity {
+//        return .init(
+//            title: title,
+//            targetWeek: targetWeek,
+//            startDate: startDate,
+//            weeklyPlans: weeklyPlans.toEntity(),
+//            user: user
+//        )
+//    }
+//}
