95b92ca4257650d159d313c1a7967e4784903bf0
domb <80871083+DONG-WOON@users.noreply.github.com>
2023-10-05 16:33:17 +0900

    Create: Extension+DateFormatter
    - 캘린더의 Date 월(Feb, 2월)로 format을 위해 DateFormatter Extension 추가

---
+++ StickToIt/Extension/Extension+DateFormatter.swift
@@ -0,0 +1,16 @@
+//
+//  Extension+DateFormatter.swift
+//  StickToIt
+//
+//  Created by 서동운 on 10/4/23.
+//
+
+import Foundation
+
+extension DateFormatter {
+    static let monthYearFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM yyyy"
+        return formatter
+    }()
+}
