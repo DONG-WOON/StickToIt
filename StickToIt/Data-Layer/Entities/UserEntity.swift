f00274802eacf9da3ba00f3fbeb02dc2bc8a8fe3
domb <80871083+DONG-WOON@users.noreply.github.com>
2023-10-23 14:07:01 +0900

    Delete: lastCertifyingDay 제거
    - 속성으로 만들고 따로 관리할 필요가 없어서 제거

--- StickToIt/Data-Layer/Entities/UserEntity.swift
+++ StickToIt/Data-Layer/Entities/UserEntity.swift
@@ -12,19 +12,16 @@ final class UserEntity: Object {
     @Persisted(primaryKey: true) var _id: UUID
     @Persisted var name: String
     @Persisted var planQueries: List<PlanQueryEntity>
-    @Persisted var lastCertifyingDay: Date?
     
     convenience init(
         _id: UUID,
         name: String,
-        planQueries: List<PlanQueryEntity>,
-        lastCertifyingDay: Date?
-        
+        planQueries: List<PlanQueryEntity>
     ) {
         self.init()
+        
         self._id = _id
         self.name = name
         self.planQueries = planQueries
-        self.lastCertifyingDay = lastCertifyingDay
     }
 }
