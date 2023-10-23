e26f86fe54eae3eb97526a666e840bf3191b28de
domb <80871083+DONG-WOON@users.noreply.github.com>
2023-10-23 14:14:07 +0900

    Update: GradientCircleView 내부에 percentage Label 추가

--- StickToIt/Presentation-Layer/DesignComponents/GradientCircleView.swift
+++ StickToIt/Presentation-Layer/DesignComponents/GradientCircleView.swift
@@ -10,6 +10,15 @@ import MKRingProgressView
 
 class GradientCircleView: RingProgressView {
     
+    let percentageLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: FontSize.body)
+        label.textColor = .assetColor(.black)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundRingColor = .assetColor(.accent3).withAlphaComponent(0.8)
@@ -17,10 +26,20 @@ class GradientCircleView: RingProgressView {
         self.endColor = .assetColor(.accent1)
         self.ringWidth = 20
         self.progress = 0.0
+        
+        self.addSubview(percentageLabel)
+        
+        percentageLabel.snp.makeConstraints { make in
+            make.center.equalTo(self)
+            make.width.lessThanOrEqualTo(self).multipliedBy(0.5)
+        }
     }
     
     func setProgress(_ progress: Double) {
         self.progress = progress
+        
+        let percentage = Int(progress * 100)
+        percentageLabel.text = "목표 달성률\n\(percentage)%"
     }
     
     required init?(coder: NSCoder) {
